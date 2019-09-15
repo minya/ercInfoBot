e5465207083e1e550e5028d218749308ac14b940
helluvasatan <m@brel.me>
2019-09-15 22:57:21 +0500

    Working with multiple accounts +Tests

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -13,250 +12,55 @@ import (
 	"github.com/minya/telegram"
 )
 
-var settings BotSettings
-var storage model.FirebaseStorage
-
-func handle(upd telegram.Update) interface{} {
-	log.Printf("Update: %v\n", upd)
-	userID := upd.Message.From.Id
-
-	userInfo, userInfoErr := storage.GetUserInfo(userID)
-
-	if nil != userInfoErr {
-		log.Printf("Login not found for user %v. Creating stub.\n", userID)
-		storage.SaveUser(userID, userInfo)
-	} else {
-		log.Printf("Login for user %v found: %v\n", userID, userInfo.Login)
-	}
-
-	cmd, cmdParseErr := ParseCommand(upd.Message.Text)
-
-	if cmdParseErr != nil {
-		log.Printf("Error parse command: %v\n", cmdParseErr)
-		return help(upd)
-	}
-
-	log.Printf("Process command: %v\n", cmd.Command)
-
-	switch cmd.Command {
-	case "/reg":
-		return register(upd, cmd.Args[0], cmd.Args[1], cmd.Args[2])
-	case "/notify":
-		return setUpNotification(upd, userInfo, cmd.Args[0] == "on")
-	case "/get":
-		return get(upd, userInfo)
-	case "/receipt":
-		return receipt(upd, userInfo)
-	default:
-		log.Printf("Unknown command: %v\n", cmd.Command)
-		return help(upd)
-	}
-}
-
-func register(upd telegram.Update, login string, password string, account string) interface{} {
-	balanceInfo, errBalanceInfo := getBalanceInfo(login, password, account)
-	if errBalanceInfo != nil {
-		return telegram.ReplyMessage{
-			ChatId: upd.Message.Chat.Id,
-			Text:   "Wrong login/password. Please, register: /reg <login> <password> <account>",
-		}
-	}
-
-	var userInfo model.UserInfo
-	userInfo.Login = login
-	userInfo.Password = password
-	userInfo.Account = account
-
-	storage.SaveUser(upd.Message.From.Id, userInfo)
-
-	return telegram.ReplyMessage{
-		ChatId: upd.Message.Chat.Id,
-		Text: fmt.Sprintf("You have been registered. "+
-			"Your balance is: %v", balanceInfo.AtTheEnd.Total),
-		ReplyMarkup: replyButtons(),
-	}
-}
-
-func get(upd telegram.Update, userInfo model.UserInfo) interface{} {
-	log.Printf("USERINFO %v\n", userInfo)
-	if userInfo.Login == "" {
-		return telegram.ReplyMessage{
-			ChatId: upd.Message.Chat.Id,
-			Text:   "Please, register: /reg <login> <password> <account>",
-		}
-	}
-
-	balanceInfo, _ := getBalanceInfo(userInfo.Login, userInfo.Password, userInfo.Account)
-	return telegram.ReplyMessage{
-		ChatId:      upd.Message.Chat.Id,
-		Text:        formatBalance(balanceInfo),
-		ReplyMarkup: replyButtons(),
-	}
-}
-
-func receipt(upd telegram.Update, userInfo model.UserInfo) interface{} {
-	receipt, _ := erclib.GetReceipt(userInfo.Login, userInfo.Password, userInfo.Account)
-	return telegram.ReplyDocument{
-		ChatId:  upd.Message.Chat.Id,
-		Caption: "Квитанция",
-		InputFile: telegram.InputFile{
-			Content:  receipt,
-			FileName: "receipt.pdf",
-		},
-		ReplyMarkup: replyButtons(),
-	}
-}
-
-func setUpNotification(upd telegram.Update, userInfo model.UserInfo, turnOn bool) telegram.ReplyMessage {
-	balanceInfo, err := getBalanceInfo(
-		userInfo.Login, userInfo.Password, userInfo.Account)
-	var lastSeenState string
-	if err == nil {
-		lastSeenState = fmt.Sprintf("%v", balanceInfo)
-	}
-	userID := upd.Message.From.Id
-	user, err := storage.GetUserInfo(userID)
-	if err != nil {
-		return telegram.ReplyMessage{
-			ChatId:      upd.Message.Chat.Id,
-			Text:        "Ошибка",
-			ReplyMarkup: replyButtons(),
-		}
-	}
-
-	user.Subscription = model.SubscriptionInfo{
-		ChatId:        upd.Message.Chat.Id,
-		LastSeenState: lastSeenState,
-	}
-
-	storage.SaveUser(userID, user)
-
-	return telegram.ReplyMessage{
-		ChatId:      upd.Message.Chat.Id,
-		Text:        "Вы подписаны на уведомления",
-		ReplyMarkup: replyButtons(),
-	}
-}
-
-func getBalanceInfo(login string, password string, accNumber string) (erclib.BalanceInfo, error) {
-	bal, err := erclib.GetBalanceInfo(login, password, accNumber, time.Now())
-	if nil != err {
-		return erclib.BalanceInfo{}, err
-	}
-	return bal, nil
-}
-
-func formatBalance(balance erclib.BalanceInfo) string {
-	return fmt.Sprintf(
-		"%v\nНачислено: %v\nПоступления: %v\nИтого: %v",
-		balance.Month,
-		formatBalanceRow(balance.Credit),
-		formatBalanceRow(balance.Debit),
-		formatBalanceRow(balance.AtTheEnd))
-}
-
-func formatBalanceRow(row erclib.Details) string {
-	return fmt.Sprintf(
-		"%v (УК: %v, Капремонт: %v)",
-		row.Total, row.CompanyPart, row.RepairPart)
-}
-
-func help(upd telegram.Update) telegram.ReplyMessage {
-	helpMsg :=
-		"/reg $login $password $account -- register your account\n" +
-			"/receipt -- get receipt (pdf)\n" +
-			"/get -- get balance\n" +
-			"/notify $on|$off -- set up notifications"
-
-	return telegram.ReplyMessage{
-		ChatId: upd.Message.Chat.Id,
-		Text:   helpMsg,
-	}
-}
-
-func updateLoop(sleepDuration time.Duration) {
-	for true {
-		log.Printf("Update...\n")
-		subsMap, err := storage.GetUsers()
-		if err != nil {
-			log.Printf("Error: %v\n", err)
-		} else {
-			for id, userInfo := range subsMap {
-				log.Printf("[Update] Check user %v\n", id)
-				if userInfo.Subscription.ChatId == 0 {
-					log.Printf("[Update] User %v is not subscribed. Skip.\n", id)
-					continue
-				}
-				balanceInfo, err := getBalanceInfo(
-					userInfo.Login, userInfo.Password, userInfo.Account)
-				if err != nil {
-					log.Printf("[Update] Error: can't get balance for user %v\n", id)
-					continue
-				}
-				newState := fmt.Sprintf("%v", balanceInfo)
-
-				sub := &userInfo.Subscription
-				if sub.LastSeenState == "" {
-					sub.LastSeenState = newState
-					storage.SaveUser(id, userInfo)
-					log.Printf("[Update] Initial balance correction for user %v\n", id)
-				} else if sub.LastSeenState != newState {
-					log.Printf("[Update] Balance changed for user %v\n", id)
-					sub.LastSeenState = newState
-					storage.SaveUser(id, userInfo)
-
-					messageText := "Баланс обновился:\n" + formatBalance(balanceInfo)
-					msg := telegram.ReplyMessage{
-						ChatId:      userInfo.Subscription.ChatId,
-						Text:        messageText,
-						ReplyMarkup: replyButtons(),
-					}
-					err = telegram.SendMessage(settings.ID, msg)
-					if err != nil {
-						fmt.Printf("%v\n", err)
-					}
-				} else {
-					log.Printf("[Update] Balance hasn't been changed\n")
-				}
-			}
-		}
-		time.Sleep(sleepDuration)
-	}
-}
-
 func main() {
-	errCfg := config.UnmarshalJson(&settings, "~/.ercInfoBot/settings.json")
-	if nil != errCfg {
-		log.Printf("Unable to get config: %v\n", errCfg)
-		return
+	settings, storage, updateCheckPeriod := initialize()
+	ntf := notifier{botToken: settings.ID, storage: storage, sleepDuration: updateCheckPeriod}
+	ntf.Start()
+	var makeERCClient = func(l string, p string) ercclient {
+		return erclib.NewErcClientWithCredentials(l, p)
 	}
+	h := createHandler(storage, makeERCClient)
+	listenErr := telegram.StartListen(settings.ID, 8080, h.handle)
+	if nil != listenErr {
+		log.Printf("Unable to start listen: %v\n", listenErr)
+	}
+}
 
-	duration, errParseDuration := time.ParseDuration(settings.UpdateCheckPeriod)
+func initialize() (BotSettings, model.FirebaseStorage, time.Duration) {
+	var settings BotSettings
+	var storage model.FirebaseStorage
+	var updateCheckPeriod time.Duration
+	var logPath string
+	flag.StringVar(&logPath, "logpath", "ercInfoBot.log", "Path to write logs")
+	var configPath string
+	flag.StringVar(&configPath, "cfg", "~/.ercInfoBot/settings.json", "Path to write logs")
+	flag.Parse()
+	setUpLogger(logPath)
+
+	errCfg := config.UnmarshalJson(&settings, configPath)
+
+	if nil != errCfg {
+		panic("Unable to get config")
+	}
+	log.Printf("Config read: %v\n", settings)
+
+	var errParseDuration error
+	updateCheckPeriod, errParseDuration = time.ParseDuration(settings.UpdateCheckPeriod)
 	if errParseDuration != nil {
 		log.Fatalf("Unable to parse duration from '%v' \n", settings.UpdateCheckPeriod)
 	}
 	if !settings.areValid() {
 		log.Fatalf("Incorrect settings: %v\n", settings)
+		panic("Incorrect settings")
 	}
+
 	fbSettings := settings.StorageSettings
 	storage = model.NewFirebaseStorage(
 		fbSettings.BaseURL,
 		fbSettings.APIKey,
 		fbSettings.Login,
 		fbSettings.Password)
-
-	go updateLoop(duration)
-	listenErr := telegram.StartListen(settings.ID, 8080, handle)
-	if nil != listenErr {
-		log.Printf("Unable to start listen: %v\n", listenErr)
-	}
-}
-
-func init() {
-	var logPath string
-	flag.StringVar(&logPath, "logpath", "ercInfoBot.log", "Path to write logs")
-	flag.Parse()
-	setUpLogger(logPath)
+	return settings, storage, updateCheckPeriod
 }
 
 func setUpLogger(logPath string) {
@@ -288,7 +92,7 @@ type BotSettings struct {
 
 func (theSettings BotSettings) areValid() bool {
 	fbSettings := &theSettings.StorageSettings
-	return settings.ID != "" &&
+	return theSettings.ID != "" &&
 		fbSettings.APIKey != "" &&
 		fbSettings.BaseURL != "" &&
 		fbSettings.Login != "" &&
